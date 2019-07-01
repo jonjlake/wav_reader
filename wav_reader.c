63d44e07baa0fec7ec1c47e4f164f809643708d5
Jon <jonjlake@gmail.com>
2019-07-01 03:38:34 -0500

    Updating JUNK and bext chunk processing, overall int storage

--- wav_reader.c
+++ wav_reader.c
@@ -28,31 +28,87 @@
 #include <math.h>
 #include "wav_reader.h"
 
+void read_string(FILE *fp, char *output_location, int chars_to_read)
+{
+	int i;
+	for (i = 0; i < chars_to_read; i++)
+	{
+		output_location[i] = getc(fp);
+	}
+	output_location[chars_to_read + 1] = '\0';
+}
+
+/* Tweak this to read in a little-endian number correctly */
+void read_number(FILE *fp, int *output_location, long int *full_output_location, int ints_to_read)
+{
+	int i;
+	*full_output_location = 0;
+	for (i = 0; i < ints_to_read; i++)
+	{
+		output_location[ints_to_read - i - 1] = getc(fp);
+		(*full_output_location) += (output_location[ints_to_read - i - 1] << (i * 8));
+//		printf("Index: %d, Full: %ld\n", output_location[ints_to_read - i - 1], *full_output_location);
+	}
+}
+
+void kill_junk(FILE *fp, int bytes_to_kill)
+{
+	int i = 0;
+	int temp;
+
+	for (i = 0; i < bytes_to_kill; i++)
+	{
+		temp = getc(fp);
+	}
+
+	printf("Killed %d bytes. Expected: %d\n", i, bytes_to_kill);
+}
+
+void kill_all_zeros(FILE *fp)
+{
+	int i = 0;
+	int temp;
+
+	do
+	{
+		temp = getc(fp);
+		i++;
+	} while('\0' == temp);
+
+	printf("Killed %d chars\n", i);
+}
+
 void read_wave(WaveFile *p_wavefile, char *filename)
 {
 	FILE *fp = fopen(filename, "r"); // Double check the open type
 	int i;
-
-	/* Read 1st 4 chars into riff_marker. Apparently strings aren't backwards */
-	for (i = 0; i < 4; i++)
+	read_string(fp, p_wavefile->riff_marker, 4);
+	read_number(fp, p_wavefile->file_size_arr, &p_wavefile->file_size, 4);
+	read_string(fp, p_wavefile->file_type_header, 4);
+	read_string(fp, p_wavefile->format_chunk_marker, 4);
+	read_number(fp, p_wavefile->format_data_length_arr, &p_wavefile->format_data_length, 4);
+	if (strcmp(p_wavefile->format_chunk_marker, "JUNK") == 0)
 	{
-		p_wavefile->riff_marker[i] = getc(fp);
+		kill_junk(fp, p_wavefile->format_data_length_arr[3]);
+		read_string(fp, p_wavefile->format_chunk_marker, 4);
+		read_number(fp, p_wavefile->format_data_length_arr, &p_wavefile->format_data_length, 4);
 	}
-
-	p_wavefile->riff_marker[4] = '\0';
-
-	/* 5-8 go into file_size */
-	for (i = 0; i < 4; i++)
+	if (strcmp(p_wavefile->format_chunk_marker, "bext") == 0)
 	{
-		p_wavefile->file_size[3 - i] = getc(fp);
+//		kill_all_zeros(fp);
+		kill_junk(fp, 602);
+//		kill_junk(fp, p_avefile->format_data_length, 4);
+		read_string(fp, p_wavefile->format_chunk_marker, 4);
+		read_number(fp, p_wavefile->format_data_length_arr, &p_wavefile->format_data_length, 4);
 	}
-
-	/* 9-12 go into file_type_header */
-	for (i = 0; i < 4; i++)
-	{
-		p_wavefile->file_type_header[i] = getc(fp);
-	}
-	p_wavefile->file_type_header[4] = '\0';
+	read_number(fp, p_wavefile->format_type_arr, &p_wavefile->format_type, 2);
+	read_number(fp, p_wavefile->num_channels_arr, &p_wavefile->num_channels, 2);
+	read_number(fp, p_wavefile->sample_rate_arr, &p_wavefile->sample_rate, 4);
+	read_number(fp, p_wavefile->sample_math_arr, &p_wavefile->sample_math, 4);
+	read_number(fp, p_wavefile->bitrate_math_arr, &p_wavefile->bitrate_math, 2);
+	read_number(fp, p_wavefile->bits_per_sample_arr, &p_wavefile->bits_per_sample, 2);
+	read_string(fp, p_wavefile->data_chunk_header, 4);
+	read_number(fp, p_wavefile->data_section_size_arr, &p_wavefile->data_section_size, 4);
 
 	fclose(fp);
 }
@@ -68,13 +124,38 @@ int main()
 	read_wave(&wavefile, filename);
 
 	printf("RIFF marker: %s\n", wavefile.riff_marker);
-	printf("file_size: %d %d %d %d\n", wavefile.file_size[0], wavefile.file_size[1], 
-			wavefile.file_size[2], wavefile.file_size[3]);
+//	printf("file_size: %d %d %d %d\n", wavefile.file_size_arr[0], wavefile.file_size_arr[1], 
+//			wavefile.file_size_arr[2], wavefile.file_size_arr[3]);
+	printf("File size: %ld\n", wavefile.file_size);
 	printf("File type header: %s\n", wavefile.file_type_header);
+	printf("Format chunk marker: %s\n", wavefile.format_chunk_marker);
+//	printf("Format data length: %d %d %d %d\n", wavefile.format_data_length_arr[0], 
+//			wavefile.format_data_length_arr[1], wavefile.format_data_length_arr[2], 
+//			wavefile.format_data_length_arr[3]);
+	printf("Format data length: %ld\n", wavefile.format_data_length);
+//	printf("Format type: %d %d\n", wavefile.format_type_arr[0], wavefile.format_type_arr[1]);
+	printf("Format type: %ld\n", wavefile.format_type);
+//	printf("Number of channels: %d %d\n", wavefile.num_channels_arr[0], wavefile.num_channels_arr[1]);
+	printf("Number of channels: %ld\n", wavefile.num_channels);
+//	printf("Sample rate: %d %d %d %d\n", wavefile.sample_rate_arr[0], wavefile.sample_rate_arr[1], 
+//			wavefile.sample_rate_arr[2], wavefile.sample_rate_arr[3]);
+	printf("Sample rate: %ld\n", wavefile.sample_rate);
+//	printf("Sample math: %d %d %d %d\n", wavefile.sample_math_arr[0], wavefile.sample_math_arr[1], 
+//			wavefile.sample_math_arr[2], wavefile.sample_math_arr[3]);
+	printf("Sample math: %ld\n", wavefile.sample_math);
+//	printf("Bitrate math: %d %d\n", wavefile.bitrate_math_arr[0], wavefile.bitrate_math_arr[1]);
+	printf("Bitrate math: %ld\n", wavefile.bitrate_math);
+//	printf("Bits per sample: %d %d\n", wavefile.bits_per_sample_arr[0], wavefile.bits_per_sample_arr[1]);
+	printf("Bits per sample: %ld\n", wavefile.bits_per_sample);
+	printf("Data chunk header: %s\n", wavefile.data_chunk_header);
+//	printf("Data section size: %d %d %d %d\n", wavefile.data_section_size_arr[0], 
+//			wavefile.data_section_size_arr[1], wavefile.data_section_size_arr[2],
+//		       	wavefile.data_section_size_arr[3]);
+	printf("Data section size: %ld\n", wavefile.data_section_size);
 
 	for (i = 0; i < 3; i++)
 	{
-		file_size_calc += (wavefile.file_size[3 - i] << (i * 8));
+		file_size_calc += (wavefile.file_size_arr[3 - i] << (i * 8));
 	}
 
 	printf("Calculated file size: %lld\n", file_size_calc);
--- wav_reader.h
+++ wav_reader.h
@@ -2,16 +2,25 @@
 typedef struct WaveFile
 {
 	char riff_marker[5];
-	int file_size[4];
+	int file_size_arr[4];
+	long int file_size;
 	char file_type_header[5];
-	char format_chunk_maker[4];
-	int format_data_length[4];
-	int format_type[2];
-	int num_channels[2];
-	int sample_rate[4];
-	int sample_math[4]; // ???
-	int bitrate_math[2]; // ???
-	int bits_per_sample[2];
+	char format_chunk_marker[5];
+	int format_data_length_arr[4];
+	long int format_data_length;
+	int format_type_arr[2];
+	long int format_type;
+	int num_channels_arr[2];
+	long int num_channels;
+	int sample_rate_arr[4];
+	long int sample_rate;
+	int sample_math_arr[4]; // ???
+	long int sample_math;
+	int bitrate_math_arr[2]; // ???
+	long int bitrate_math;
+	int bits_per_sample_arr[2];
+	long int bits_per_sample;
 	char data_chunk_header[5];
-	int data_section_size[4];
+	int data_section_size_arr[4];
+	long int data_section_size;
 } WaveFile;
